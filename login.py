7ebdb80a01a10b5f972b6eb65d4cc7e87bf6c617
disonto <disonto@kookmin.ac.kr>
2022-11-25 23:24:23 +0900

    주석 수정

--- login.py
+++ login.py
@@ -1,7 +1,6 @@
 # 로그인 정보를 확인하는 파일
-# https://sikidocs.net/85739(참고?)
-# main으로부터 받은 id,pw를 on국민에 넣었을 때의 웹페이지를 크롤링하여 국민대 학생인지 확인한다
-# view-source:https://portal.kookmin.ac.kr/por/ln -570line
+# https://stackoverflow.com/questions/2910221/how-can-i-login-to-a-website-with-python
+# view-source:https://ecampus.kookmin.ac.kr/login/index.php
 # view-source:https://sso.kookmin.ac.kr/sso/error.jsp
 from PyQt5.QtWidgets import QMessageBox
 
